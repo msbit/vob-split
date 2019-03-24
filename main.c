714ccb5cdb7676dff34c8d8880b0758c9492495c
Tom Sullivan <tom@msbit.com.au>
2019-03-24 19:17:36 +1100

    Determine relevant VOB files extents

--- main.c
+++ main.c
@@ -2,11 +2,17 @@
 #include <dvdread/ifo_read.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <string.h>
+#include <sys/stat.h>
 
 struct extent_t {
   uint32_t first_sector;
   uint32_t last_sector;
 };
+
+#define DVD_SECTOR_SIZE 2048
 
 int populate_pgc_extents(pgcit_t *vts_pgcit, struct extent_t **pgc_extents) {
   *pgc_extents = malloc(sizeof(struct extent_t) * vts_pgcit->nr_of_pgci_srp);
@@ -19,6 +25,43 @@ int populate_pgc_extents(pgcit_t *vts_pgcit, struct extent_t **pgc_extents) {
   }
 
   return vts_pgcit->nr_of_pgci_srp;
+}
+
+int populate_vob_extents(char *path, int title, struct extent_t **vob_extents) {
+  *vob_extents = malloc(sizeof(struct extent_t) * 10);
+
+  DIR *d;
+  struct dirent *dir;
+  d = opendir(path);
+  if (d == NULL) {
+    perror("opendir");
+    exit(-6);
+  }
+
+  char match_prefix[20];
+  char nomatch_prefix[20];
+
+  snprintf(match_prefix, 20, "VTS_%02d_", title);
+  snprintf(nomatch_prefix, 20, "VTS_%02d_0", title);
+
+  uint32_t first_sector = 0;
+  int index = 0;
+  while((dir = readdir(d)) != NULL) {
+    if (dir->d_type == DT_REG
+      && (strstr(dir->d_name, match_prefix) == dir->d_name)
+      && (strstr(dir->d_name, nomatch_prefix) != dir->d_name)) {
+      struct stat st;
+      char filename[1024];
+      snprintf(filename, 1024, "%s/%s", path, dir->d_name);
+      stat(filename, &st);
+
+      uint32_t last_sector = first_sector + (st.st_size / DVD_SECTOR_SIZE);
+      (*vob_extents)[index++] = (struct extent_t){first_sector, last_sector - 1};
+      first_sector = last_sector;
+    }
+  }
+  closedir(d);
+  return index;
 }
 
 int main(int argc, char **argv) {
@@ -55,10 +98,16 @@ int main(int argc, char **argv) {
 
   int pgc_extent_count = populate_pgc_extents(ifo->vts_pgcit, &pgc_extents);
 
+  struct extent_t *vob_extents;
+
+  int vob_extent_count = populate_vob_extents(path, title, &vob_extents);
+
   for (int i = 0; i < pgc_extent_count; i++) {
     printf("%d %u %u\n", i, pgc_extents[i].first_sector, pgc_extents[i].last_sector);
   }
 
+  for (int i = 0; i < vob_extent_count; i++) {
+    printf("%d %u %u\n", i, vob_extents[i].first_sector, vob_extents[i].last_sector);
   }
 
   DVDClose(dvd);
