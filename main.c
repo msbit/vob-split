689c59ae283747429d334d9a00be9d2b60bbcee8
Tom Sullivan <tom@msbit.com.au>
2019-03-24 19:17:11 +1100

    Use unified extent_t type

--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct pgc_extent_t {
+struct extent_t {
   uint32_t first_sector;
   uint32_t last_sector;
 };
 
-void populate_pgc_extents(pgcit_t *vts_pgcit, struct pgc_extent_t **pgc_extents) {
-  *pgc_extents = malloc(sizeof(struct pgc_extent_t) * vts_pgcit->nr_of_pgci_srp);
+int populate_pgc_extents(pgcit_t *vts_pgcit, struct extent_t **pgc_extents) {
+  *pgc_extents = malloc(sizeof(struct extent_t) * vts_pgcit->nr_of_pgci_srp);
 
   for (int i = 0; i < vts_pgcit->nr_of_pgci_srp; i++) {
     pgc_t *pgc = vts_pgcit->pgci_srp[i].pgc;
     uint32_t first_sector = pgc->cell_playback[0].first_sector;
     uint32_t last_sector = pgc->cell_playback[pgc->nr_of_cells - 1].last_sector;
-    (*pgc_extents)[i] = (struct pgc_extent_t){first_sector, last_sector};
+    (*pgc_extents)[i] = (struct extent_t){first_sector, last_sector};
   }
+
+  return vts_pgcit->nr_of_pgci_srp;
 }
 
 int main(int argc, char **argv) {
@@ -49,12 +51,14 @@ int main(int argc, char **argv) {
     exit(-5);
   }
 
-  struct pgc_extent_t *pgc_extents;
+  struct extent_t *pgc_extents;
 
-  populate_pgc_extents(ifo->vts_pgcit, &pgc_extents);
+  int pgc_extent_count = populate_pgc_extents(ifo->vts_pgcit, &pgc_extents);
 
-  for (int i = 0; i < ifo->vts_pgcit->nr_of_pgci_srp; i++) {
-    printf("%d %d %d\n", i, pgc_extents[i].first_sector, pgc_extents[i].last_sector);
+  for (int i = 0; i < pgc_extent_count; i++) {
+    printf("%d %u %u\n", i, pgc_extents[i].first_sector, pgc_extents[i].last_sector);
+  }
+
   }
 
   DVDClose(dvd);
